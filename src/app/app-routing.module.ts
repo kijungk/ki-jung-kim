e1616dba67f1d5e7e116b8cc0b43ecb6da905136
Ki Jung Kim <kimkijung@icloud.com>
2020-01-12 20:08:55 -1000

    added app routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,33 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./pages/home/home.component";
+import { AboutComponent } from "./pages/about/about.component";
+import { SkillsetComponent } from "./pages/skillset/skillset.component";
+import { ProjectsComponent } from "./pages/projects/projects.component";
+import { ConnectComponent } from "./pages/connect/connect.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent, data: { animation: "Home" } },
+  { path: "about", component: AboutComponent, data: { animation: "About" } },
+  {
+    path: "skillset",
+    component: SkillsetComponent,
+    data: { animation: "Skillset" }
+  },
+  {
+    path: "projects",
+    component: ProjectsComponent,
+    data: { animation: "Projects" }
+  },
+  {
+    path: "connect",
+    component: ConnectComponent,
+    data: { animation: "Connect" }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
