ffa5cb53e4df4b8c41f96950ccc6fb84cf75cd0e
Ki Jung Kim <kimkijung@icloud.com>
2020-01-12 20:07:16 -1000

    added icon service to register custom mat-icon using svg files

---
+++ src/app/services/icon/icon.service.ts
@@ -0,0 +1,28 @@
+import { DomSanitizer } from "@angular/platform-browser";
+import { MatIconRegistry } from "@angular/material/icon";
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class IconService {
+  private icons: string[] = ["angular", "github", "linkedin"];
+
+  private basePath = "../../assets/icons/";
+
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {}
+
+  public registerCustomIcons(): void {
+    this.icons.forEach(icon => {
+      this.matIconRegistry.addSvgIcon(
+        icon,
+        this.domSanitizer.bypassSecurityTrustResourceUrl(
+          this.basePath + icon + ".svg"
+        )
+      );
+    });
+
+    return;
+  }
+}
